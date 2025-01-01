b5801dece8f3652e033ae1eac4ec31fb82be0ca9
reem <reemmohmedmohmed@gmail.com>
2025-01-01 15:23:01 +0200

    creat Either Backage  for home repose in doman and creat failuers

--- lib/Features/Home/Domain/repos/home_repo.dart
+++ lib/Features/Home/Domain/repos/home_repo.dart
@@ -1,6 +1,10 @@
 import 'package:bookly/Features/Home/Domain/entities/book_entity.dart';
+import 'package:bookly/core/Error/failers.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFutchersBooks();
-  Future<List<BookEntity>> fetchNewssBooks();
+  Future<Either<Failers, List<BookEntity>>> fetchFetchersBooks();
+
+  // Future<List<BookEntity>> fetchFutchersBooks();
+  // Future<List<BookEntity>> fetchNewssBooks();
 }
