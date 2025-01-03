da648345930fe512bc8045eddaccb95b59ec984b
reem <reemmohmedmohmed@gmail.com>
2025-01-03 17:28:00 +0200

    craet adabtor hive for BookEntity

--- lib/Features/Home/Domain/entities/book_entity.dart
+++ lib/Features/Home/Domain/entities/book_entity.dart
@@ -1,9 +1,19 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookid;
+  @HiveField(1)
   final String image;
+  @HiveField(2)
   final String titel;
+  @HiveField(3)
   final String auther;
+  @HiveField(4)
   final num price;
+  @HiveField(5)
   final num rationg;
 
   BookEntity(
