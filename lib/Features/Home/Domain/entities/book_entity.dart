f5ec9eed36e89631bee770544d25add7f25d5ce3
reem <reemmohmedmohmed@gmail.com>
2025-01-03 12:26:15 +0200

    creat data model and i make  extends BookEntity for bookModel and ui change the data object for Model int to num or dynamic

--- lib/Features/Home/Domain/entities/book_entity.dart
+++ lib/Features/Home/Domain/entities/book_entity.dart
@@ -1,9 +1,16 @@
 class BookEntity {
+  final String bookid;
   final String image;
   final String titel;
   final String auther;
   final num price;
   final num rationg;
 
-  BookEntity(this.image, this.titel, this.auther, this.price, this.rationg);
+  BookEntity(
+      {required this.bookid,
+      required this.image,
+      required this.titel,
+      required this.auther,
+      required this.price,
+      required this.rationg});
 }
