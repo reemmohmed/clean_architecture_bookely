240d69264efafe14be92e076bcbe1546c34864fd
reem <reemmohmedmohmed@gmail.com>
2025-01-05 13:01:58 +0200

    implement cubit  for fetchfetchersBooks

--- lib/Features/Home/presentaion/manger/FeachFeatersBooks/fetuers_books_cubit.dart
+++ lib/Features/Home/presentaion/manger/FeachFeatersBooks/fetuers_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/Home/Domain/Use%20Cases/fetch_featuers_books_cas.dart';
 import 'package:bookly/Features/Home/Domain/entities/book_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'fetuers_books_state.dart';
 
 class FetuersBooksCubit extends Cubit<FetuersBooksState> {
-  FetuersBooksCubit() : super(FetuersBooksInitial());
+  FetuersBooksCubit(this.fetchFeatuersBooksCas) : super(FetuersBooksInitial());
+  final FetchFeatuersBooksCas fetchFeatuersBooksCas;
+
+  Future<void> feachFeachedBooks() async {
+    emit(FetuersBooksLoding());
+    var result = await fetchFeatuersBooksCas.call();
+    result.fold((failers) {
+      emit(FetuersBooksFailers(failers.erroeMessage));
+    }, (books) {
+      emit(FetuersBooksScusess(books));
+    });
+  }
 }
